4e8e7a651ad2fceb0baa73666efdb44dd56cf63d
YTY2001 <xoxo88851@naver.com>
2025-07-03 18:03:57 +0900

    Add files via upload

---
+++ java/연습문제/MemberEXample.java
@@ -0,0 +1,9 @@
+package ch11.sec01;
+
+public class MemberEXample {
+    public static void main(String[] args) {
+        Member member =new Member("blue", "이파란");
+        System.out.println(member);
+    }
+    
+}
