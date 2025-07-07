d6a2d3a2743bae5f62cfd651e18357fcb47642e0
YTY2001 <xoxo88851@naver.com>
2025-07-07 17:36:37 +0900

    Add files via upload

--- java/연습문제/20250707/3번/MovieThread.java
+++ java/연습문제/20250707/3번/MovieThread.java
@@ -1,5 +1,15 @@
 package ex12_3;
 
-public class MovieThread {
+public class MovieThread extends Thread {
+    @Override
+    public void run(){
+        while (true) {
+         System.out.println("동영상을 재생합니다."); 
+         if (this.isInterrupted()) {
+            break;
+            
+         }  
+        }
+    }
 
 }
--- java/연습문제/20250707/3번/ThreadExample.java
+++ java/연습문제/20250707/3번/ThreadExample.java
@@ -1,5 +1,14 @@
 package ex12_3;
 
+import ch122.MovieThread;
+
 public class ThreadExample {
+    public static void main(String[] args) {
+        Thread thread =new MovieThread();
+        thread.start();
+
+        try{Thread.sleep(1000);}catch (InterruptedException e){}
+        thread.interrupt();
+    }
 
 }
